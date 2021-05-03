1c1a7338f477b2826d20d057ba57fa24cf7dc265
vivi-1 <wwei6@vt.edu>
2021-05-03 10:51:19 -0400

    Deseq2 Installation

--- Deseq2.R
+++ Deseq2.R
@@ -1,1 +1,7 @@
-
+##Installation of Deseq2
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install()
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install("DESeq2")
