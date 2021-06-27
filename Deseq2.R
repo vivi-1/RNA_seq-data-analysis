747b9c1c396ae8a5e240a2d2e865d15de67c7d91
vivi-1 <wwei6@vt.edu>
2021-06-27 18:34:10 -0400

    change

--- Deseq2.R
+++ Deseq2.R
@@ -8,15 +8,18 @@
 #BiocManager::install("limma")
 #BiocManager::install("apeglm")
 
-lilbrary(readxl)
+library(readxl)
 library (DeSeq2)
 library(limma)
 library(apeglm)  
 library(ggplot2)
 
-setwd('/Users/weiwang/Desktop/DEGlist/Output')
-readscount <- read_excel('/Users/weiwang/Desktop/DEGlist/Input/raw data_readcount_no rep2.xlsx', sheet = "gene.description")
-colData <- read_excel('/Users/weiwang/Desktop/DEGlist/Input/colData_no rep2.xlsx', sheet = "Sheet1")
+setwd('/Volumes/WD1/Desktop/laboratory files/Results/Altria project/DEGlist/Wei_reDEGlist/Output')
+readscount <- read_excel('/Volumes/WD1/Desktop/laboratory files/Results/Altria project/DEGlist/Wei_reDEGlist/Input/raw data_readcount_no rep2.xlsx', sheet = "gene.description")
+geneIDs<-read_excel('/Volumes/WD1/Desktop/laboratory files/Results/Altria project/DEGlist/Wei_reDEGlist/Input/Gene_ID_list.xlsx', sheet = "Sheet1")
+row.names(readscount) <- geneIDs$geneID #Assigning row names from geneIDs 
+
+colData <- read_excel('/Volumes/WD1/Desktop/laboratory files/Results/Altria project/DEGlist/Wei_reDEGlist/Input/colData_no rep2.xlsx', sheet = "Sheet1")
 condition <-factor(c(rep(c("Control", "Flag22", "Pnic", "Flag22+Pnic"), 17), "Control", "Flag22", "Pnic", rep(c("Control","Flag22", "Pnic", "Flag22+Pnic"), 10)))
 timepoint <- factor(c(rep(c(rep("T1", 4), rep("T2", 4), rep("T3", 4), rep("T4", 4), rep("T5", 4), rep("T6", 4), rep("T7", 4)), 2), 
                         rep("T1", 4), rep("T2", 4), rep("T3", 4), rep("T4", 3), rep("T5", 4), rep("T6", 4), rep("T7", 4),
@@ -33,9 +36,9 @@ keep <- rowSums(counts(dds) >= 10) >= 3
 dds <- dds[keep, ]
 
 #PCA analysis###
-#vsdata <- vst(dds, blind=FALSE) #variance stabilizing transformation
-#assay(vsdata) <- limma::removeBatchEffect(assay(vsdata), vsdata$timepoint, vsdata$condition) 
-#plotPCA(vsdata, intgroup = "replicate")
+vsdata <- vst(dds, blind=FALSE) #variance stabilizing transformation
+assay(vsdata) <- limma::removeBatchEffect(assay(vsdata), vsdata$timepoint, vsdata$condition) 
+plotPCA(vsdata, intgroup = "replicate")
 
 vsdata <- vst(dds, blind=FALSE)
 assay(vsdata) <- limma::removeBatchEffect(assay(vsdata), vsdata$timepoint, vsdata$replicate) 
@@ -56,9 +59,14 @@ hist(readscount_new, main = "Readscount Histgram")
 # Differential expression analysis between each treatment and control
 dds_norm <- DESeq(dds, minReplicatesForReplace = Inf) #Normalization; outliers is not filtered out; same result with cookscutoff
 dds_norm$condition   #make sure levels are treat/untreat，otherwise needs to explicitely say it when define results
-#res_Pnic <- results(dds_norm, contrast = c("condition","Pnic","Control"), cooksCutoff = FALSE) #alpha=0.05 for padj; cookCutoff (for filter out outlier)= false cuz there are lots of outlier
-#res_Flag22 <- results(dds_norm, contrast = c("condition","Flag22","Control"), cooksCutoff = FALSE) #alpha=0.05 for padj; cookCutoff (for filter out outlier)= false cuz there are lots of outlier
+res_Pnic <- results(dds_norm, contrast = c("condition","Pnic","Control"), cooksCutoff = FALSE) #alpha=0.05 for padj; cookCutoff (for filter out outlier)= false cuz there are lots of outlier
+dds_norm <- DESeq(dds, minReplicatesForReplace = Inf) #Normalization; outliers is not filtered out; same result with cookscutoff
+dds_norm$condition   #make sure levels are treat/untreat，otherwise needs to explicitely say it when define results
+res_Flag22 <- results(dds_norm, contrast = c("condition","Flag22","Control"), cooksCutoff = FALSE) #alpha=0.05 for padj; cookCutoff (for filter out outlier)= false cuz there are lots of outlier
+dds_norm <- DESeq(dds, minReplicatesForReplace = Inf) #Normalization; outliers is not filtered out; same result with cookscutoff
+dds_norm$condition   #make sure levels are treat/untreat，otherwise needs to explicitely say it when define results
 res_Flag22_Pnic <- results(dds_norm, contrast = c("condition","Flag22+Pnic","Control"), cooksCutoff = FALSE) #alpha=0.05 for padj; cookCutoff (for filter out outlier)= false cuz there are lots of outlier
+
 summary(res_Pnic) 
 summary(res_Flag22)
 summary(res_Flag22_Pnic)
@@ -101,6 +109,16 @@ write.csv(down_Flag22_PnicDEG, "down_Flag22_Pnic_no rep2.csv")
 par(mar=c(8,5,2,2))
 boxplot(log10(assays(dds_norm)[["cooks"]]), range=0, las=2, outbg = "green",outpch = 25)
 
+#Venn plot
+df1<-read.csv("all_Pnic_no rep2.csv",header = T,stringsAsFactors = F)
+df2<-read.csv("down_Pnic_no rep2.csv",header = T,stringsAsFactors = F)
+df3<-read.csv("up_Pnic_no rep2.csv",header = T,stringsAsFactors = F)
+library(ggvenn)
+x<-list(all=df1$x,down=df2$x,up=df3$x)
+ggvenn(x,c("all","up","down"),set_name_size = 3,fill_alpha = 1,text_size = 3)
+
+
+
 # MA plot
 dds_norm <- DESeq(dds, minReplicatesForReplace = Inf) #Normalization; outliers is not filtered out; same result with cookscutoff
 resultsNames(dds_norm)  #shrink makes data more compact,don't change padj，change foldchange
@@ -126,37 +144,57 @@ dev.off()
 voldata_Flag22 <-read.csv(file = "all_Flag22_no rep2.csv",header = TRUE, row.names =1)
 voldata_Pnic <-read.csv(file = "all_Pnic_no rep2.csv",header = TRUE, row.names =1)
 voldata_Flag22_Pnic <-read.csv(file = "all_Flag22_Pnic_no rep2.csv",header = TRUE, row.names =1)
+voldata_Flag22$change<-as.factor(ifelse(
+  voldata_Flag22$padj<0.01 & abs(voldata_Flag22$log2FoldChange)>1,
+  ifelse(voldata_Flag22$log2FoldChange>1, "Up", "Down"),
+  "No Diff"
+)
+)
+voldata_Pnic$change<-as.factor(ifelse(
+  voldata_Pnic$padj<0.01 & abs(voldata_Pnic$log2FoldChange)>1,
+  ifelse(voldata_Pnic$log2FoldChange>1, "Up", "Down"),
+  "No Diff"
+)
+)
 
+voldata_Flag22_Pnic$change<-as.factor(ifelse(
+  voldata_Flag22_Pnic$padj<0.01 & abs(voldata_Flag22_Pnic$log2FoldChange)>1,
+  ifelse(voldata_Flag22_Pnic$log2FoldChange>1, "Up", "Down"),
+  "No Diff"
+)
+)
 
 pdf("volcano.pdf", width = 6.13, height = 5.18)
-ggplot(data=voldata_Flag22, aes(x=log2FoldChange,y= -1*log10(padj))) +
-  geom_point(aes(color='significant')) +
-  scale_color_manual(values=c("#546de5", "#d2dae2","#ff4757")) + 
+ggplot(data=voldata_Flag22, aes(x=log2FoldChange,y= -1*log10(padj), color=change)) +
+  geom_point(alpha=0.8, size=1) +
+  scale_color_manual(values=c("red", "green","black"), limits=c("Up", "Down", "NoDiff")) + 
   labs(title="Volcano Plot_Flag22: ", x=expression(log[2](FC), y=expression(-log[10](padj)))) +
-  geom_hline(yintercept=1.3,linetype=4) +  
-  geom_vline(xintercept=c(-1,1),linetype=4) +
-  theme_bw() + theme(panel.grid = element_blank()) 
-ggplot(data=voldata_Pnic, aes(x=log2FoldChange,y= -1*log10(padj))) +
-  geom_point(aes(color='significant')) +
-  scale_color_manual(values=c("#546de5", "#d2dae2","#ff4757")) + 
+  geom_hline(yintercept=-log10(0.01),linetype=4, col="gray", lwd=0.5) +  
+  geom_vline(xintercept=c(-1,1),linetype=4, col="gray", lwd=0.5) +
+  theme_bw(base_size=15) + theme(panel.grid = element_blank()) 
+
+ggplot(data=voldata_Pnic, aes(x=log2FoldChange,y= -1*log10(padj), color=change)) +
+  geom_point(alpha=0.8, size=1) +
+  scale_color_manual(values=c("red", "green","black"), limits=c("Up", "Down", "NoDiff")) + 
   labs(title="Volcano Plot_Pnic: ", x=expression(log[2](FC), y=expression(-log[10](padj)))) +
-  geom_hline(yintercept=1.3,linetype=4) +  
-  geom_vline(xintercept=c(-1,1),linetype=4) +
-  theme_bw() + theme(panel.grid = element_blank())  
-ggplot(data=voldata_Flag22_Pnic, aes(x=log2FoldChange,y= -1*log10(padj))) +
-  geom_point(aes(color='significant')) +
-  scale_color_manual(values=c("#546de5", "#d2dae2","#ff4757")) + 
-  labs(title="Volcano Plot_Flag22+Pnic: ", x=expression(log[2](FC), y=expression(-log[10](padj)))) +
-  geom_hline(yintercept=1.3,linetype=4) + 
-  geom_vline(xintercept=c(-1,1),linetype=4) +
-  theme_bw() + theme(panel.grid = element_blank())  
+  geom_hline(yintercept=-log10(0.01),linetype=4, col="gray", lwd=0.5) +  
+  geom_vline(xintercept=c(-1,1),linetype=4, col="gray", lwd=0.5) +
+  theme_bw(base_size=15) + theme(panel.grid = element_blank()) 
+
+ggplot(data=voldata_Flag22_Pnic, aes(x=log2FoldChange,y= -1*log10(padj), color=change)) +
+  geom_point(alpha=0.8, size=1) +
+  scale_color_manual(values=c("red", "green","black"), limits=c("Up", "Down", "NoDiff")) + 
+  labs(title="Volcano Plot_Pnic: ", x=expression(log[2](FC), y=expression(-log[10](padj)))) +
+  geom_hline(yintercept=-log10(0.01),linetype=4, col="gray", lwd=0.5) +  
+  geom_vline(xintercept=c(-1,1),linetype=4, col="gray", lwd=0.5) +
+  theme_bw(base_size=15) + theme(panel.grid = element_blank()) 
 
 dev.off()
 
 
 ### T4
-readscount <- read_excel('/Users/weiwang/Desktop/DEGlist/Input/raw data_readcount_no rep2.xlsx', sheet = "T4")
-colData <- read_excel('/Users/weiwang/Desktop/DEGlist/Input/colData_no rep2.xlsx', sheet = "T4")
+readscount <- read_excel('/Volumes/WD1/Desktop/laboratory files/Results/Altria project/DEGlist/Wei_reDEGlist/Input/raw data_readcount_no rep2.xlsx', sheet = "T4")
+colData <- read_excel('/Volumes/WD1/Desktop/laboratory files/Results/Altria project/DEGlist/Wei_reDEGlist/Input/colData_no rep2.xlsx', sheet = "T4")
 condition <-factor(c(c("Control", "Flag22", "Pnic", "Flag22+Pnic"), c("Control", "Flag22", "Pnic"), c("Control", "Flag22", "Pnic", "Flag22+Pnic")))
 replicate <- factor(c(rep("Rep1", 4), rep("Rep3", 3), rep("Rep0", 4)))
 colData
@@ -266,34 +304,57 @@ voldata_Flag22 <-read.csv(file = "T4_all_Flag22_no rep2.csv",header = TRUE, row.
 voldata_Pnic <-read.csv(file = "T4_all_Pnic_no rep2.csv",header = TRUE, row.names =1)
 voldata_Flag22_Pnic <-read.csv(file = "T4_all_Flag22_Pnic_no rep2.csv",header = TRUE, row.names =1)
 
+voldata_Flag22$change<-as.factor(ifelse(
+                                        voldata_Flag22$padj<0.01 & abs(voldata_Flag22$log2FoldChange)>1,
+                                        ifelse(voldata_Flag22$log2FoldChange>1, "Up", "Down"),
+                                        "No Diff"
+                                        )
+                                 )
+voldata_Pnic$change<-as.factor(ifelse(
+                                    voldata_Pnic$padj<0.01 & abs(voldata_Pnic$log2FoldChange)>1,
+                                    ifelse(voldata_Pnic$log2FoldChange>1, "Up", "Down"),
+                                    "No Diff"
+                                    )
+                              )
+
+voldata_Flag22_Pnic$change<-as.factor(ifelse(
+                                      voldata_Flag22_Pnic$padj<0.01 & abs(voldata_Flag22_Pnic$log2FoldChange)>1,
+                                      ifelse(voldata_Flag22_Pnic$log2FoldChange>1, "Up", "Down"),
+                                      "No Diff"
+                                      )
+                                )
+
 
 pdf("T4_volcano.pdf", width = 6.13, height = 5.18)
-ggplot(data=voldata_Flag22, aes(x=log2FoldChange,y= -1*log10(padj))) +
-  geom_point(aes(color='significant')) +
-  scale_color_manual(values=c("#546de5", "#d2dae2","#ff4757")) + 
+ggplot(data=voldata_Flag22, aes(x=log2FoldChange,y= -1*log10(padj), color=change)) +
+  geom_point(alpha=0.8, size=1) +
+  scale_color_manual(values=c("red", "green","black"), limits=c("Up", "Down", "NoDiff")) + 
   labs(title="T4_Volcano Plot_Flag22: ", x=expression(log[2](FC), y=expression(-log[10](padj)))) +
-  geom_hline(yintercept=1.3,linetype=4) +  
-  geom_vline(xintercept=c(-1,1),linetype=4) +
-  theme_bw() + theme(panel.grid = element_blank()) 
-ggplot(data=voldata_Pnic, aes(x=log2FoldChange,y= -1*log10(padj))) +
-  geom_point(aes(color='significant')) +
-  scale_color_manual(values=c("#546de5", "#d2dae2","#ff4757")) + 
+  geom_hline(yintercept=-log10(0.01),linetype=4, col="gray", lwd=0.5) +  
+  geom_vline(xintercept=c(-1,1),linetype=4, col="gray", lwd=0.5) +
+  theme_bw(base_size=15) + theme(panel.grid = element_blank()) 
+
+ggplot(data=voldata_Pnic, aes(x=log2FoldChange,y= -1*log10(padj), color=change)) +
+  geom_point(alpha=0.8, size=1) +
+  scale_color_manual(values=c("red", "green","black"), limits=c("Up", "Down", "NoDiff")) + 
   labs(title="T4_Volcano Plot_Pnic: ", x=expression(log[2](FC), y=expression(-log[10](padj)))) +
-  geom_hline(yintercept=1.3,linetype=4) + 
-  geom_vline(xintercept=c(-1,1),linetype=4) +
-  theme_bw() + theme(panel.grid = element_blank())  
-ggplot(data=voldata_Flag22_Pnic, aes(x=log2FoldChange,y= -1*log10(padj))) +
-  geom_point(aes(color='significant')) +
-  scale_color_manual(values=c("#546de5", "#d2dae2","#ff4757")) + 
-  labs(title="T4_Volcano Plot_Flag22+Pnic: ", x=expression(log[2](FC), y=expression(-log[10](padj)))) +
-  geom_hline(yintercept=1.3,linetype=4) + 
-  geom_vline(xintercept=c(-1,1),linetype=4) +
-  theme_bw() + theme(panel.grid = element_blank())  
+  geom_hline(yintercept=-log10(0.01),linetype=4, col="gray", lwd=0.5) +  
+  geom_vline(xintercept=c(-1,1),linetype=4, col="gray", lwd=0.5) +
+  theme_bw(base_size=15) + theme(panel.grid = element_blank()) 
+
+ggplot(data=voldata_Flag22_Pnic, aes(x=log2FoldChange,y= -1*log10(padj), color=change)) +
+  geom_point(alpha=0.8, size=1) +
+  scale_color_manual(values=c("red", "green","black"), limits=c("Up", "Down", "NoDiff")) + 
+  labs(title="T4_Volcano Plot_Pnic: ", x=expression(log[2](FC), y=expression(-log[10](padj)))) +
+  geom_hline(yintercept=-log10(0.01),linetype=4, col="gray", lwd=0.5) +  
+  geom_vline(xintercept=c(-1,1),linetype=4, col="gray", lwd=0.5) +
+  theme_bw(base_size=15) + theme(panel.grid = element_blank()) 
+
 dev.off()
 
 ### T5
-readscount <- read_excel('/Users/weiwang/Desktop/DEGlist/Input/raw data_readcount_no rep2.xlsx', sheet = "T5")
-colData <- read_excel('/Users/weiwang/Desktop/DEGlist/Input/colData_no rep2.xlsx', sheet = "T5")
+readscount <- read_excel('/Volumes/WD1/Desktop/laboratory files/Results/Altria project/DEGlist/Wei_reDEGlist/Input/raw data_readcount_no rep2.xlsx', sheet = "T5")
+colData <- read_excel('/Volumes/WD1/Desktop/laboratory files/Results/Altria project/DEGlist/Wei_reDEGlist/Input/colData_no rep2.xlsx', sheet = "T5")
 condition <-factor(c(rep(c("Control", "Flag22", "Pnic", "Flag22+Pnic"), 3)))
 replicate <- factor(c(rep("Rep1", 4), rep("Rep3", 4), rep("Rep0", 4)))
 colData
@@ -403,28 +464,50 @@ voldata_Flag22 <-read.csv(file = "T5_all_Flag22_no rep2.csv",header = TRUE, row.
 voldata_Pnic <-read.csv(file = "T5_all_Pnic_no rep2.csv",header = TRUE, row.names =1)
 voldata_Flag22_Pnic <-read.csv(file = "T5_all_Flag22_Pnic_no rep2.csv",header = TRUE, row.names =1)
 
+voldata_Flag22$change<-as.factor(ifelse(
+  voldata_Flag22$padj<0.01 & abs(voldata_Flag22$log2FoldChange)>1,
+  ifelse(voldata_Flag22$log2FoldChange>1, "Up", "Down"),
+  "No Diff"
+)
+)
+voldata_Pnic$change<-as.factor(ifelse(
+  voldata_Pnic$padj<0.01 & abs(voldata_Pnic$log2FoldChange)>1,
+  ifelse(voldata_Pnic$log2FoldChange>1, "Up", "Down"),
+  "No Diff"
+  )
+)
+
+voldata_Flag22_Pnic$change<-as.factor(ifelse(
+  voldata_Flag22_Pnic$padj<0.01 & abs(voldata_Flag22_Pnic$log2FoldChange)>1,
+  ifelse(voldata_Flag22_Pnic$log2FoldChange>1, "Up", "Down"),
+  "No Diff"
+  )
+)
 
 pdf("T5_volcano.pdf", width = 6.13, height = 5.18)
-ggplot(data=voldata_Flag22, aes(x=log2FoldChange,y= -1*log10(padj))) +
-  geom_point(aes(color='significant')) +
-  scale_color_manual(values=c("#546de5", "#d2dae2","#ff4757")) + 
+ggplot(data=voldata_Flag22, aes(x=log2FoldChange,y= -1*log10(padj), color=change)) +
+  geom_point(alpha=0.8, size=1) +
+  scale_color_manual(values=c("red", "green","black"), limits=c("Up", "Down", "NoDiff")) + 
   labs(title="T5_Volcano Plot_Flag22: ", x=expression(log[2](FC), y=expression(-log[10](padj)))) +
-  geom_hline(yintercept=1.3,linetype=4) +  
-  geom_vline(xintercept=c(-1,1),linetype=4) +
-  theme_bw() + theme(panel.grid = element_blank())  
-ggplot(data=voldata_Pnic, aes(x=log2FoldChange,y= -1*log10(padj))) +
-  geom_point(aes(color='significant')) +
-  scale_color_manual(values=c("#546de5", "#d2dae2","#ff4757")) + 
+  geom_hline(yintercept=-log10(0.01),linetype=4, col="gray", lwd=0.5) +  
+  geom_vline(xintercept=c(-1,1),linetype=4, col="gray", lwd=0.5) +
+  theme_bw(base_size=15) + theme(panel.grid = element_blank()) 
+
+ggplot(data=voldata_Pnic, aes(x=log2FoldChange,y= -1*log10(padj), color=change)) +
+  geom_point(alpha=0.8, size=1) +
+  scale_color_manual(values=c("red", "green","black"), limits=c("Up", "Down", "NoDiff")) + 
   labs(title="T5_Volcano Plot_Pnic: ", x=expression(log[2](FC), y=expression(-log[10](padj)))) +
-  geom_hline(yintercept=1.3,linetype=4) + 
-  geom_vline(xintercept=c(-1,1),linetype=4) +
-  theme_bw() + theme(panel.grid = element_blank())
-ggplot(data=voldata_Flag22_Pnic, aes(x=log2FoldChange,y= -1*log10(padj))) +
-  geom_point(aes(color='significant')) +
-  scale_color_manual(values=c("#546de5", "#d2dae2","#ff4757")) + 
-  labs(title="T5_Volcano Plot_Flag22+Pnic: ", x=expression(log[2](FC), y=expression(-log[10](padj)))) +
-  geom_hline(yintercept=1.3,linetype=4) +  
-  geom_vline(xintercept=c(-1,1),linetype=4) +
-  theme_bw() + theme(panel.grid = element_blank()) 
+  geom_hline(yintercept=-log10(0.01),linetype=4, col="gray", lwd=0.5) +  
+  geom_vline(xintercept=c(-1,1),linetype=4, col="gray", lwd=0.5) +
+  theme_bw(base_size=15) + theme(panel.grid = element_blank()) 
+
+ggplot(data=voldata_Flag22_Pnic, aes(x=log2FoldChange,y= -1*log10(padj), color=change)) +
+  geom_point(alpha=0.8, size=1) +
+  scale_color_manual(values=c("red", "green","black"), limits=c("Up", "Down", "NoDiff")) + 
+  labs(title="T5_Volcano Plot_Pnic: ", x=expression(log[2](FC), y=expression(-log[10](padj)))) +
+  geom_hline(yintercept=-log10(0.01),linetype=4, col="gray", lwd=0.5) +  
+  geom_vline(xintercept=c(-1,1),linetype=4, col="gray", lwd=0.5) +
+  theme_bw(base_size=15) + theme(panel.grid = element_blank()) 
+
 dev.off()
 
