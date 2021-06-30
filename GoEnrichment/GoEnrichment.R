b177458a09b8e63c0386d29e4d6b9144e578cdf8
vivi-1 <wwei6@vt.edu>
2021-06-30 03:34:29 -0400

    Finish

---
+++ GoEnrichment/GoEnrichment.R
@@ -0,0 +1,74 @@
+library("clusterProfiler")
+# import gene lists
+PnicUpgene <- read.csv("/Volumes/WD1/Desktop/laboratory files/Results/Altria project/DEGlist/Wei_reDEGlist/Output/up_Pnic_no rep2.csv",header = T,sep=",")
+PnicDowngene<-read.csv("/Volumes/WD1/Desktop/laboratory files/Results/Altria project/DEGlist/Wei_reDEGlist/Output/down_Pnic_no rep2.csv",header = T,sep=",")
+FlagUpgene<-read.csv("/Volumes/WD1/Desktop/laboratory files/Results/Altria project/DEGlist/Wei_reDEGlist/Output/up_Flag22_no rep2.csv",header = T,sep=",")
+FlagDowngene<-read.csv("/Volumes/WD1/Desktop/laboratory files/Results/Altria project/DEGlist/Wei_reDEGlist/Output/down_Flag22_no rep2.csv",header = T,sep=",")
+Flag_PnicUpgene<-read.csv("/Volumes/WD1/Desktop/laboratory files/Results/Altria project/DEGlist/Wei_reDEGlist/Output/up_Flag22_Pnic_no rep2.csv",header = T,sep=",")
+Flag_PnicDowngene<-read.csv("/Volumes/WD1/Desktop/laboratory files/Results/Altria project/DEGlist/Wei_reDEGlist/Output/down_Flag22_Pnic_no rep2.csv",header = T,sep=",")
+PnicUpgene<-as.factor(PnicUpgene['Row.names'])
+PnicDowngene<-as.factor(PnicDowngene['Row.names'])
+FlagUpgene<-as.factor(FlagUpgene['Row.names'])
+FlagDowngene<-as.factor(FlagDowngene['Row.names'])
+Flag_PnicUpgene<-as.factor(Flag_PnicUpgene['Row.names'])
+Flag_PnicDowngene<-as.factor(Flag_PnicDowngene['Row.names'])
+
+
+#import annotation
+term2gene <- read.csv("/Users/weiwang/Desktop/GoEnrichment/GOterms annotations.csv",header=T,sep=",")
+term2name <- read.csv("/Users/weiwang/Desktop/GoEnrichment/GOterms_GeneList1.csv",header=F,sep=",")
+
+# Enrichment
+xPnicUpgene <- enricher(PnicUpgene,TERM2GENE=term2gene,TERM2NAME=term2name,pvalueCutoff = 0.01, pAdjustMethod = "BH", qvalueCutoff = 0.05)
+ouf <- paste(out_file,sep ="\t")
+# result
+write.csv(xPnicUpgene,ouf)
+# barplot
+barplot(x)
+# bubble plot
+dotplot(x)
+
+xPnicDowngene <- enricher(PnicDowngene,TERM2GENE=term2gene,TERM2NAME=term2name,pvalueCutoff = 0.01, pAdjustMethod = "BH", qvalueCutoff = 0.05)
+ouf <- paste(out_file,sep ="\t")
+# result
+write.csv(xPnicDowngene,ouf)
+# barplot
+barplot(x)
+# bubble plot
+dotplot(x)
+
+xFlagUpgene <- enricher(FlagUpgene,TERM2GENE=term2gene,TERM2NAME=term2name,pvalueCutoff = 0.01, pAdjustMethod = "BH", qvalueCutoff = 0.05)
+ouf <- paste(out_file,sep ="\t")
+# result
+write.csv(xFlagUpgene,ouf)
+# barplot
+barplot(x)
+# bubble plot
+dotplot(x)
+
+xFlagDowngene <- enricher(FlagDowngene,TERM2GENE=term2gene,TERM2NAME=term2name,pvalueCutoff = 0.01, pAdjustMethod = "BH", qvalueCutoff = 0.05)
+ouf <- paste(out_file,sep ="\t")
+# result
+write.csv(xFlagDowngene,ouf)
+# barplot
+barplot(x)
+# bubble plot
+dotplot(x)
+
+xFlag_PnicUpgene <- enricher(Flag_PnicUpgene,TERM2GENE=term2gene,TERM2NAME=term2name,pvalueCutoff = 0.01, pAdjustMethod = "BH", qvalueCutoff = 0.05)
+ouf <- paste(out_file,sep ="\t")
+# result
+write.csv(xFlag_PnicUpgene,ouf)
+# barplot
+barplot(x)
+# bubble plot
+dotplot(x)
+
+xFlag_PnicDowngene <- enricher(Flag_PnicDowngene,TERM2GENE=term2gene,TERM2NAME=term2name,pvalueCutoff = 0.01, pAdjustMethod = "BH", qvalueCutoff = 0.05)
+ouf <- paste(out_file,sep ="\t")
+# result
+write.csv(xFlag_PnicDowngene,ouf)
+# barplot
+barplot(x)
+# bubble plot
+dotplot(x)
--- HeatMap/HeatMap.R
+++ HeatMap/HeatMap.R
@@ -1,6 +1,30 @@
 library(pheatmap)
 library(readxl)
+library(RColorBrewer)
 # xls files
-heatmap_data<- read_excel("my_file.xls")
-# xlsx files
-my_data <- read_excel("my_file.xlsx")
+options(scipen=999) #prevent scientific notations
+options (future.globals.maxSize = 4000 * 1024^7) #increase memory consumption
+
+#vector memory exhausted limit error:
+#(from terminal)
+#cd ~
+#touch .Renviron
+#open .Renviron
+#add R_MAX_VSIZE=100Gb to  .Renviron file
+
+#Use SSH to access tinkercliff and do the map there
+#https://video.vt.edu/media/ARCA+Accessing+clusters+from+the+command+line+via+SSH/1_nkojfb72/176584251
+
+temp<- read_excel("PnicHeatMapData.xlsx", sheet = "Pnic")
+heatmap_data <- temp[,-1]
+row.names(heatmap_data) <- temp$Gene_ID
+summary(c(heatmap_data))
+
+color<-colorRampPalette(c('#436eee','white','#EE0000'))(100)
+png(filename = "Pnic_vs_Water.png", height=4000, width=3000, res=500, units="px")
+anno_col=data.frame(sampleType=factor(rep(c('Mock','Pnic'), each=21)))
+ann_color=list(sampleType=c(mock='#cd0000', normal='#3a5fcd'))
+pheatmap(heatmap_data, scale="none", color=color, annotation_col=anno_col, annotation_colors = ann_color,
+         fontsize = 8,fontsize_row = 4,show_colnames = F, main = "Pnic vs Mock heatmap",
+         legend_breaks=c(4,12),legend_labels = c('low','high'))
+dev.off()
