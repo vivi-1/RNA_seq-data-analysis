9a219fb66c0a7150963970dad9f5ce8556675726
vivi-1 <wwei6@vt.edu>
2021-07-09 23:05:15 -0400

    Finish

--- GoEnrichment/GoEnrichment.R
+++ GoEnrichment/GoEnrichment.R
@@ -1,5 +1,6 @@
 library("clusterProfiler")
 library(enrichR)
+library(ggplot2)
 
 #import annotation
 contents <- read.csv("/Users/weiwang/Desktop/GoEnrichment/Pnic_up_GO.csv",header=T,sep=",")
@@ -16,6 +17,16 @@ ouf <- paste('Pnic_up_Enricher.csv',sep ="\t")
 # result
 write.csv(xPnicUpgene,ouf)
 # barplot
-barplot(xPnicUpgene,showCategory=20, includeAll=TRUE)
+barplot(xPnicUpgene,showCategory=35, includeAll=TRUE, cex.names=0.5)
 # bubble plot
-dotplot(xPnicUpgene,showCategory=20)
+dotplot(xPnicUpgene,showCategory=35)
+#enrich plot
+xPnicUpgene<-enrichplot::pairwise_termsim(xPnicUpgene)
+emapplot(xPnicUpgene,  showCategory = 30, cex_label_category=0.4)
+g<-cnetplot(xPnicUpgene, showCategory = 20,cex_label_category = 1, cex_label_gene=0.8)
+g + scale_color_manual(values = c("red","blue"))
+
+GOI <- read.csv("/Users/weiwang/Desktop/GoEnrichment/Output/Pnic_up_Enricher.csv",header=T,sep=",")
+dotplot(as.vector(t(GOI)))
+
+browseKEGG(kk, "hsa04934")
